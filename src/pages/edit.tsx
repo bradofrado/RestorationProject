b9477db25f830089d73d3f97f4e8e84378ec5f0b
Braydon Jones <bradofrado@gmail.com>
2023-05-08 22:24:39 -0600

    Add ability to add editable components

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,25 +1,43 @@
 import { type NextPage } from "next";
 import Link from "next/link";
-import { type FocusEvent, useState, useRef, useEffect, PropsWithChildren, DOMAttributes, InputHTMLAttributes } from "react";
+import { useState } from "react";
+import AddComponent, { type onDeleteComponent } from "~/utils/components/AddComponent";
 import Editable from "~/utils/components/Editable";
 
 const Edit_page: NextPage = () => {
+	const [components, setComponents] = useState<React.ElementType<onDeleteComponent>[]>([]);
+
+	const onAdd = (component: React.ElementType<onDeleteComponent>) => {
+		const copy = components.slice();
+		copy.push(component);
+		setComponents(copy);
+	}
+
+	const deleteComponent = (index: number) => {
+		const copy = components.slice();
+		copy.splice(index, 1);
+		setComponents(copy);
+	}
 	return <>
 		<div className="max-w-4xl px-4 mx-auto sm:px-24 my-10">
 			<Link href="/timeline" className="text-xs italic font-bold text-gray-600 no-underline uppercase hover:text-gray-800">&lt; Back to timeline</Link>
-			<Editable as="h1" className="hover:bg-sky-200/50 p-2 rounded-md mx-auto text-3xl font-bold my-5 text-bom">
+			<Editable as="h1" className="mx-auto text-3xl font-bold my-5 text-bom">
 				Book of Mormon Translation
 			</Editable>
 			<div>
-				<Editable as="p" className="hover:bg-sky-200/50 p-2 rounded-md">
+				<Editable as="p">
 					This is a description
 				</Editable>
 			</div>
 			<div className="py-10">
 				<h2 className="text-xl font-bold">Timeline of Events</h2>
 			</div>
+			{components.map((Component: React.ElementType<onDeleteComponent>, i: number) => <Component key={i} onDelete={() => deleteComponent(i)}/>)}
+			<AddComponent onAdd={onAdd}/>
 		</div>
 	</>
 }
+
+
 
 export default Edit_page;
--- src/utils/components/Editable.tsx
+++ src/utils/components/Editable.tsx
@@ -1,7 +1,10 @@
 import { type PolymorphicComponentProps } from "../types/polymorphic";
+import { DeleteIcon } from "./icons/icons";
 
 interface EditableProps {
 	children: string,
+	className?: string,
+	onDelete?: () => void
 }
 
 type TextProps<C extends React.ElementType> = PolymorphicComponentProps<
@@ -9,12 +12,23 @@ type TextProps<C extends React.ElementType> = PolymorphicComponentProps<
   EditableProps
 >
 
-function Editable<T extends React.ElementType>({children, as, ...rest}: TextProps<T>) {
+function Editable<T extends React.ElementType>({children, as, className, onDelete, ...rest}: TextProps<T>) {
 	const Component = as || 'span';
+
+	const classAll: string = 'hover:bg-sky-200/50 p-2 rounded-md peer' + (className || '');
 	
-	return <> 
-		<Component {...rest} contentEditable="true">{children}</Component>
-	</>
+	return <div className="relative"> 
+		
+		<Component {...rest} contentEditable="true" className={classAll}>{children}</Component>
+		{onDelete && <button className="transition-all peer-focus:opacity-100 opacity-0 invisible peer-focus:visible hover:visible absolute -top-8 rounded-md bg-slate-50 hover:bg-slate-300 p-1"
+			onClick={onDelete}
+		>
+			<DeleteIcon className="h-5 w-5"/>
+		</button>}
+	</div>
 }
+
+
+
 
 export default Editable;
