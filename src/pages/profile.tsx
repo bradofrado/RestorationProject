aeb911d45bd2d17f610718a60d26c627278f1568
Braydon Jones <bjones@lucidchart.com>
2023-06-17 23:15:33 -0600

    Make simple profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,11 @@
 import { type GetServerSideProps, type GetServerSidePropsContext, type NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
 import { getServerAuthSession } from "~/server/auth";
+import Button from "~/utils/components/base/button";
+import Header from "~/utils/components/base/header";
 import { Hyperlink } from "~/utils/components/base/hyperlink";
+import Panel from "~/utils/components/base/panel";
+import { ProfileIcon } from "~/utils/components/icons/icons";
 
 const requireAuth =
   (func: GetServerSideProps) => async (ctx: GetServerSidePropsContext) => {
@@ -30,8 +34,16 @@ const Profile_page: NextPage = () => {
     }
 
     return <>
-        Hello {data.user.name}
-        <Hyperlink onClick={() => void signOut()}>Logout</Hyperlink>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <Panel className="mx-auto max-w-md w-full">
+                <div className="py-10 flex flex-col items-center space-y-2">
+                    <ProfileIcon className="h-10 w-10"/>
+                    <Header className="pt-5">{data.user.name}</Header>
+                    <p>{data.user.email}</p>
+                    <Hyperlink onClick={() => void signOut()}>Logout</Hyperlink>
+                </div>
+            </Panel>
+        </div>
     </>
 }
 
