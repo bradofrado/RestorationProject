21e502fe77e4fcc196d07c590bd188226f001c84
braydonjones <braydon@joinleland.com>
2025-09-05 16:42:52 -0600

    improvement: don't show html encoded characters like &amp;

--- src/utils/components/blocks/annotation/annotation-markdown.tsx
+++ src/utils/components/blocks/annotation/annotation-markdown.tsx
@@ -1,7 +1,8 @@
-import { FC, Fragment, ReactNode, useMemo } from 'react';
+import { type FC, Fragment, type ReactNode, useMemo } from 'react';
 import { inlineAnnotationRegex } from './constants';
 import { useAnnotationLink } from '../../event-page/annotation-provider';
 import { useAnnotationComponent } from './annotation-component-provider';
+import { decode } from 'entities';
 
 interface AnnotationMarkdownProps {
   text: string;
@@ -24,7 +25,9 @@ export const AnnotationMarkdown: FC<AnnotationMarkdownProps> = ({ text }) => {
       )
     );
 
-    return nodes;
+    // The text shows up in the db as "&amp;", so we need to decode it
+    return nodes.map((c) => (typeof c === 'string' ? decode(c) : c));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [text, annotate]);
 
   return <Fragment>{content}</Fragment>;
